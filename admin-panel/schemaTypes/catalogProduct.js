86803e1452d75b09569293ccf68f8987c9362254
zverposta <zverposta@gmail.com>
2026-05-27 18:19:35 +0400

    added sorting

--- admin-panel/schemaTypes/catalogProduct.js
+++ admin-panel/schemaTypes/catalogProduct.js
@@ -5,15 +5,37 @@ export default {
   fields: [
     { name: 'title', title: 'სახელი', type: 'string' },
     { name: 'volume', title: 'მოცულობა (მლ)', type: 'string' },
-    { 
-      name: 'powerLevel', 
-      title: 'სიძლიერე (1-დან 5-მდე)', 
+    {
+      name: 'powerLevel',
+      title: 'სიძლიერე (1-დან 5-მდე)',
       type: 'string',
       description: 'ჩაწერე: l1, l2, l3, l4 ან l5 (დიზაინისთვის)'
     },
     { name: 'price', title: 'ფასი', type: 'number' },
     { name: 'description', title: 'დეტალური აღწერა', type: 'text' },
     { name: 'image', title: 'სურათი', type: 'image' },
-    { name: 'isStock', title: 'მარაგშია', type: 'boolean' }
+    { name: 'isStock', title: 'მარაგშია', type: 'boolean' },
+
+    {
+      name: 'displayLocation',
+      title: 'სად გამოჩნდეს საიტზე?',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'მხოლოდ პროდუქტებში', value: 'products' },
+          { title: 'მთავარ გვერდზეც და პროდუქტებშიც', value: 'both' },
+          { title: 'მხოლოდ მთავარ გვერდზე', value: 'home' }
+        ],
+        layout: 'radio'
+      },
+      initialValue: 'products'
+    },
+    {
+      name: 'position',
+      title: 'რიგითობის ნომერი (მაგ: 1, 2, 3...)',
+      type: 'number',
+      description: 'რაც უფრო ნაკლებს ჩაწერთ (მაგ: 1), მით უფრო წინ გამოჩნდება საიტზე.',
+      initialValue: 1
+    }
   ]
 }
